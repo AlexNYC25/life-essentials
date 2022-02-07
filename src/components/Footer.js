7f61367ad0af9ba42910631f7a95d25304c4039a
Alexis Montes <montes.alexis25@gmail.com>
2022-02-07 00:36:54 -0500

    created components for page

---
+++ src/components/Footer.js
@@ -0,0 +1,106 @@
+import { Row } from "react-bootstrap";
+
+import twitterSvg from '../icons/icons8-twitter.svg'
+import instagramSvg from '../icons/icons8-instagram.svg'
+import facebookSvg from '../icons/icons8-facebook.svg'
+
+
+export default function Footer() {
+
+    return (
+        <footer className="pt-5 bottom-0" >
+            <Row>
+                <div className="col-2">
+                    <h5>Categories</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Electronics</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Jewlery</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Men's Clothing</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Woman's Clothing</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-2">
+                    <h5>Customer Service</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Order Status</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Shipping</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Return Policy</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">FAQ</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-2">
+                    <h5>About Life Essenitals</h5>
+                    <ul className="nav flex-column">
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Humble Origins</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Meteroric Rise</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Partnerships with Designers</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">World Wide Expansion</a>
+                        </li>
+                        <li className="nav-item mb-2">
+                            <a href="#" className="nav-link p-0 text-muted">Who We Are Know</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-4 offset-1">
+                    <form>
+                        <h5>Subscribe to our newsletter</h5>
+                        <p>Find out when new products arrive and when sales start.</p>
+                        <div className="d-flex w-100 gap-2">
+                            <input id="newsletter1" type="text" class="form-control" placeholder="Email" />
+                            <button className="btn btn-primary-outline">Subscribe</button>
+                        </div>
+                    </form>
+                </div>
+            </Row>
+
+            <div className="d-flex px-1 justify-content-between border-top">
+                <p>2021 Life Essentials, Inc. All Rights Reserved.</p>
+                <ul className="list-unstyled d-flex">
+                    <li className="ms-3">
+                        <a className="link-dark" href="#">
+                            <img src={twitterSvg} alt="twitter" />
+                        </a>
+                    </li>
+
+                    <li className="ms-3">
+                        <a className="link-dark" href="#">
+                            <img src={instagramSvg} alt="instagram" />
+                        </a>
+                    </li>
+
+                    <li className="ms-3">
+                        <a className="link-dark" href="#">
+                            <img src={facebookSvg} alt="facebook" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+
+import react from "react"
+import { Navbar, NavDropdown, Nav} from "react-bootstrap"
+import {Link} from "react-router-dom"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function NavBar() {
+
+    return (
+        <Navbar className="px-2 pt-3" expand="lg">
+            <Navbar.Brand className="px-2" href="#home">Life Essentials</Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+                <Nav className="justify-content-center">
+                    <Link to="/" className="nav-link">
+                        Home
+                    </Link>
+                    <NavDropdown title="Categories" id="basic-nav-dropdown">
+                        <Link to='/category/men%27s%20clothing' className="dropdown-item">
+                            Men's Clothing
+                        </Link>
+                        <Link to="/category/women%27s%20clothing" className="dropdown-item">
+                            Women's Clothing
+                        </Link>
+                        <Link to="/category/electronics" className="dropdown-item">
+                            Electronics
+                        </Link>
+                        <Link to="/category/jewelery" className="dropdown-item">
+                            Jewlery
+                        </Link>
+                    </NavDropdown>
+                    <Link to="/" className="nav-link">
+                        About Us
+                    </Link>
+                    <Link to="/" className="nav-link">
+                        Contact Us
+                    </Link>
+                </Nav>
+
+                <Nav className="justify-content-end">
+                    <button type="button" class="btn position-relative" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            1
+                        </span>
+                    </button>
+                    
+                </Nav>
+            </Navbar.Collapse>
+            
+        </Navbar>
+    )
+}

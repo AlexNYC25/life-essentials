20469f9976b3bfdc03e4ace3c9cabc4c54718933
Alexis Montes <montes.alexis25@gmail.com>
2022-02-09 23:39:56 -0500

    created component for diaplaying product

---
+++ src/components/ProductCard.js
@@ -0,0 +1,45 @@
+import Col from "react-bootstrap/Col"
+import { Image } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
+export default function ProductCard(props){
+
+    let getStars = (rating) => {
+        let elements = []
+        let wholeStars = Math.floor(rating)
+        let halfStars = rating - wholeStars > .5 ? 1 : 0;
+        let emptyStars = 5 - wholeStars - halfStars
+
+        let wholeStarSvg = <img src="https://img.icons8.com/fluency/48/000000/filled-star.png" style={{width:'10%'}}/>
+        let halfStarSvg = <img src="https://img.icons8.com/color/48/000000/star-half-empty.png" style={{width:'10%'}}/>
+        let emptyStarSvg = <img src="https://img.icons8.com/color/48/000000/star--v1.png" style={{width:'10%'}}/>
+
+        for(let i = 0; i < wholeStars; i++){
+            elements.push(wholeStarSvg)
+        }
+        for(let i = 0; i < halfStars; i++){
+            elements.push(halfStarSvg)
+        }
+        for(let i = 0; i < emptyStars; i++){
+            elements.push(emptyStarSvg)
+        }
+        return elements
+    }
+
+    return (
+        <Col xs={12} sm={6} md={4} lg={3} xl={3} >
+            <div className="card mb-3" >
+                <Link to={'/product/'+ props.product.id} >
+                    <img src={props.product.image} key={45} className="card-img-top thumbnail" style={{width:'100%', height:'45vh', objectFit:'contain'}} />
+                    <div className="absolute-position" >
+                        {getStars(props.product.rating.rate)}
+                    </div>
+                </Link>
+                <div className="card-body product-details" style={{height:'20vh'}}>
+                    <h5 className="card-title">{props.product.title}</h5>
+                </div>
+                
+            </div>
+        </Col>
+    )
+}

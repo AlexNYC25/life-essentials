79ae3bce1ccd9fdc9ea5e5cc7d244935aae0d617
Alexis Montes <montes.alexis25@gmail.com>
2022-02-17 22:54:25 -0500

    replaced icons, fixed links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,7 @@
 import { Row } from "react-bootstrap";
 
-import twitterSvg from '../icons/icons8-twitter.svg'
-import instagramSvg from '../icons/icons8-instagram.svg'
-import facebookSvg from '../icons/icons8-facebook.svg'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
 
@@ -14,16 +12,16 @@ export default function Footer() {
                     <h5>Categories</h5>
                     <ul className="nav flex-column">
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Electronics</a>
+                            <a href="/" className="nav-link p-0 text-muted">Electronics</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Jewlery</a>
+                            <a href="/" className="nav-link p-0 text-muted">Jewlery</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Men's Clothing</a>
+                            <a href="/" className="nav-link p-0 text-muted">Men's Clothing</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Woman's Clothing</a>
+                            <a href="/" className="nav-link p-0 text-muted">Woman's Clothing</a>
                         </li>
                     </ul>
                 </div>
@@ -31,19 +29,19 @@ export default function Footer() {
                     <h5>Customer Service</h5>
                     <ul className="nav flex-column">
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Order Status</a>
+                            <a href="/" className="nav-link p-0 text-muted">Order Status</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Shipping</a>
+                            <a href="/" className="nav-link p-0 text-muted">Shipping</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Return Policy</a>
+                            <a href="/" className="nav-link p-0 text-muted">Return Policy</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">FAQ</a>
+                            <a href="/" className="nav-link p-0 text-muted">FAQ</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Contact Us</a>
+                            <a href="/" className="nav-link p-0 text-muted">Contact Us</a>
                         </li>
                     </ul>
                 </div>
@@ -51,19 +49,19 @@ export default function Footer() {
                     <h5>About Life Essenitals</h5>
                     <ul className="nav flex-column">
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Humble Origins</a>
+                            <a href="/" className="nav-link p-0 text-muted">Humble Origins</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Meteroric Rise</a>
+                            <a href="/" className="nav-link p-0 text-muted">Meteroric Rise</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Partnerships with Designers</a>
+                            <a href="/" className="nav-link p-0 text-muted">Partnerships with Designers</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">World Wide Expansion</a>
+                            <a href="/" className="nav-link p-0 text-muted">World Wide Expansion</a>
                         </li>
                         <li className="nav-item mb-2">
-                            <a href="#" className="nav-link p-0 text-muted">Who We Are Know</a>
+                            <a href="/" className="nav-link p-0 text-muted">Who We Are Know</a>
                         </li>
                     </ul>
                 </div>
@@ -83,20 +81,20 @@ export default function Footer() {
                 <p>2021 Life Essentials, Inc. All Rights Reserved.</p>
                 <ul className="list-unstyled d-flex">
                     <li className="ms-3">
-                        <a className="link-dark" href="#">
-                            <img src={twitterSvg} alt="twitter" />
+                        <a className="link-dark" href="/">
+                            <FontAwesomeIcon icon={faTwitter} size='2x'/>
                         </a>
                     </li>
 
                     <li className="ms-3">
-                        <a className="link-dark" href="#">
-                            <img src={instagramSvg} alt="instagram" />
+                        <a className="link-dark" href="/">
+                            <FontAwesomeIcon icon={faInstagram} size='2x'/>
                         </a>
                     </li>
 
                     <li className="ms-3">
-                        <a className="link-dark" href="#">
-                            <img src={facebookSvg} alt="facebook" />
+                        <a className="link-dark" href="/">
+                            <FontAwesomeIcon icon={faFacebook} size='2x'/>
                         </a>
                     </li>
                 </ul>
